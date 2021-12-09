f4fb57bd52ae7f1b9a4ddcee5a4e15de9e11008c
Ahmed Al Kindi <ahmkind19@gmail.com>
2021-12-09 22:27:17 +0400

    fixed adenture images and added jaifar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,11 @@
 import { useTranslation } from 'react-i18next'
+import Jaifar from '../assets/jaifar.png'
 
 const About = () => {
   const { t } = useTranslation()
   return (
     <div>
+      <img src={Jaifar} alt="omanosaura founder Jaifar Al Kindi" width={400} />
       {t('jaifar')} {t('jaifarDesc')} {t('companyDesc')}
     </div>
   )
