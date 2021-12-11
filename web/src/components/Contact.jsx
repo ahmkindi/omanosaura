291712732885af925b0f266824961043a04bba00
Ahmed Al Kindi <ahmkind19@gmail.com>
2021-12-11 23:42:33 +0400

    added axios for backend com

--- web/src/components/Contact.jsx
+++ web/src/components/Contact.jsx
@@ -1,6 +1,32 @@
+import { useRef, useState } from 'react'
 import styles from './contact.module.scss'
+import axios from 'axios'
 
 const Contact = () => {
+  const [formOpen, setFormOpen] = useState(false)
+  const name = useRef(null)
+  const email = useRef(null)
+  const subject = useRef(null)
+  const message = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(name.current.value)
+    console.log(message.current.value)
+
+    setFormOpen(false)
+    axios
+      .post('api/send', {
+        name: 'name',
+        email: 'ahmkind19@gmail.com',
+        subject: 'subject',
+        message: 'message',
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error))
+  }
+
   return (
     <div className={styles.contact}>
       <div className={styles.sectionheader}>
@@ -14,40 +40,46 @@ const Contact = () => {
           adventure awaits!
         </p>
 
-        <label for="checkcontact" className={styles.contactbutton}>
+        <label htmlFor="checkcontact" className={styles.contactbutton}>
           <div className={styles.mail}></div>
         </label>
         <input
           id="checkcontact"
           className={styles.checkcontact}
           type="checkbox"
+          checked={formOpen}
+          onChange={() => setFormOpen((prevValue) => !prevValue)}
         />
 
-        <form action="" method="post" className={styles.contactform}>
+        <form onSubmit={(e) => handleSubmit(e)} className={styles.contactform}>
           <p className={styles.inputWrapper}>
-            <input type="text" name="contact_nom" value="" id="contact_nom" />
-            <label for="contact_nom">NAME</label>
+            <input type="text" name="contact_nom" id="contact_nom" ref={name} />
+            <label htmlFor="contact_nom">NAME</label>
           </p>
           <p className={styles.inputWrapper}>
             <input
               type="text"
               name="contact_email"
-              value=""
               id="contact_email"
+              ref={email}
             />
-            <label for="contact_email">EMAIL</label>
+            <label htmlFor="contact_email">EMAIL</label>
           </p>
           <p className={styles.inputWrapper}>
             <input
               type="text"
               name="contact_sujet"
-              value=""
               id="contact_sujet"
+              ref={subject}
             />
-            <label for="contact_sujet">SUBJECT</label>
+            <label htmlFor="contact_sujet">SUBJECT</label>
           </p>
           <p className={styles.textareaWrapper}>
-            <textarea name="contact_message" id="contact_message"></textarea>
+            <textarea
+              name="contact_message"
+              id="contact_message"
+              ref={message}
+            ></textarea>
           </p>
           <p className={styles.submitWrapper}>
             <input type="submit" value="SEND" />
