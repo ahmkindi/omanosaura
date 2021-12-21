4e20188326d393f5ecbf726c45631a7404f5febf
Ahmed Al Kindi <ahmkind19@gmail.com>
2021-12-21 22:28:26 +0400

    auth saved in localstorage

--- web/src/components/admin/AdminHome.jsx
+++ web/src/components/admin/AdminHome.jsx
@@ -1,8 +1,11 @@
-import { useContext, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import AuthContext from '../../AuthContext'
 import { Form, Button, Alert } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import CryptoJS from 'crypto-js'
+
+const secretKey = 'iamasecretkey'
 
 const AdminHome = () => {
   const auth = useContext(AuthContext)
@@ -10,29 +13,62 @@ const AdminHome = () => {
   const username = useRef(null)
   const password = useRef(null)
 
-  const handleLogin = (e) => {
-    e.preventDefault()
-    axios
-      .post(
+  const correctLogin = async (user, pass) => {
+    try {
+      const response = await axios.post(
         '/admin/',
         {},
         {
           auth: {
-            username: username.current.value,
-            password: password.current.value,
+            username: user,
+            password: pass,
           },
         }
       )
-      .then((response) => {
-        if (response.status === 200) {
-          auth.setUsername(username.current.value)
-          auth.setPassword(password.current.value)
-          auth.setSignedIn(true)
-        } else {
-          setIncorrect(true)
-        }
-      })
-      .catch(() => setIncorrect(true))
+      if (response.status === 200) {
+        return true
+      } else {
+        return false
+      }
+    } catch (error) {
+      return false
+    }
+  }
+
+  useEffect(() => {
+    const user = localStorage.getItem('username')
+    const encPass = localStorage.getItem('password')
+    if (encPass) {
+      const bytes = CryptoJS.AES.decrypt(encPass, secretKey)
+      const pass = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+      if (correctLogin(user, pass)) {
+        auth.setUsername(user)
+        auth.setPassword(pass)
+        auth.setSignedIn(true)
+        return
+      }
+    }
+    auth.setSignedIn(false)
+  }, [auth.setSignedIn])
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    const pass = password.current.value
+    if (correctLogin(username.current.value, pass)) {
+      auth.setUsername(username.current.value)
+      auth.setPassword(pass)
+      auth.setSignedIn(true)
+      var algo = CryptoJS.algo.SHA256.create()
+      algo.update(pass, 'utf-8')
+      localStorage.setItem('username', username.current.value)
+      localStorage.setItem(
+        'password',
+        CryptoJS.AES.encrypt(JSON.stringify(pass), 'iamasecretkey').toString()
+      )
+      setIncorrect(false)
+    } else {
+      setIncorrect(true)
+    }
   }
 
   return (
