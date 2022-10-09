fe0c1b1af2c2e389af93c5388be09be4c743ea24
Ahmed Al Kindi <ahmkind19@gmail.com>
2022-10-09 23:04:47 +0400

    added react simple map

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,15 @@ import type { NextPage } from 'next'
 import useTranslation from 'next-translate/useTranslation'
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from 'react-simple-maps'
+
+const geoUrl =
+  'https://res.cloudinary.com/dl093kbg1/raw/upload/v1665340389/omn_admbnd_1_ly0tcy.json'
 
 const Home: NextPage = () => {
   const { t, lang } = useTranslation()
@@ -11,6 +20,31 @@ const Home: NextPage = () => {
       <Link href="/" locale={lang === 'ar' ? 'en' : 'ar'}>
         <h2>{t('common:language')}</h2>
       </Link>
+      <ComposableMap
+        projectionConfig={{
+          scale: 900,
+          rotate: [-15, 5, -15],
+        }}
+        width={800}
+        height={400}
+        style={{ width: '100%', height: 'auto', background: '#e1eced' }}
+      >
+        <ZoomableGroup center={[56, 21.5]} zoom={4} maxZoom={20}>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#043c6c"
+                  stroke="#e1eced"
+                  strokeWidth={0.05}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
     </Layout>
   )
 }
