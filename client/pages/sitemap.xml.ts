df2b0e8c88a5def9eb9d0125f241097cd1f983e0
Ahmed Al Kindi <ahmkind19@gmail.com>
2023-10-24 22:38:15 +0400

    added arabic to sitemaps

--- client/pages/sitemap.xml.ts
+++ client/pages/sitemap.xml.ts
@@ -5,44 +5,52 @@ import { GetServerSideProps } from 'next'
 import { BlogPreface, Product } from '../types/requests'
 
 function generateSiteMap(blogs: BlogPreface[], products: Product[]) {
+  const generateUrlWithAlternates = (path: string) => `
+    <url>
+      <loc>${base}${path}</loc>
+      <xhtml:link rel="alternate" hreflang="en" href="${base}${path}" />
+      <xhtml:link rel="alternate" hreflang="ar" href="${base}/ar${path}" />
+    </url>
+  `
+
+  const generateBlogUrls = () =>
+    blogs
+      .map(
+        ({ id }) => `
+      <url>
+        <loc>${base}/blogs/${id}</loc>
+        <xhtml:link rel="alternate" hreflang="en" href="${base}/blogs/${id}" />
+        <xhtml:link rel="alternate" hreflang="ar" href="${base}/ar/blogs/${id}" />
+      </url>
+    `
+      )
+      .join('')
+
+  const generateProductUrls = () =>
+    products
+      .map(
+        ({ id }) => `
+      <url>
+        <loc>${base}/experiences/${id}</loc>
+        <xhtml:link rel="alternate" hreflang="en" href="${base}/experiences/${id}" />
+        <xhtml:link rel="alternate" hreflang="ar" href="${base}/ar/experiences/${id}" />
+      </url>
+    `
+      )
+      .join('')
+
   return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://omanosaura.com</loc>
-     </url>
-     <url>
-       <loc>https://omanosaura.com/blogs</loc>
-     </url>
-     <url>
-       <loc>https://omanosaura.com/experiences</loc>
-     </url>
-     <url>
-       <loc>https://omanosaura.com/contact</loc>
-     </url>
-     <url>
-       <loc>https://omanosaura.com/about</loc>
-     </url>
-     ${blogs
-       .map(({ id }) => {
-         return `
-       <url>
-           <loc>${`${base}/blogs/${id}`}</loc>
-       </url>
-     `
-       })
-       .join('')}
-     ${products
-       .map(({ id }) => {
-         return `
-       <url>
-           <loc>${`${base}/experiences/${id}`}</loc>
-       </url>
-     `
-       })
-       .join('')}
-   </urlset>
- `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xhtml="http://www.w3.org/1999/xhtml">
+      ${generateUrlWithAlternates('')}
+      ${generateUrlWithAlternates('/blogs')}
+      ${generateUrlWithAlternates('/experiences')}
+      ${generateUrlWithAlternates('/contact')}
+      ${generateUrlWithAlternates('/about')}
+      ${generateBlogUrls()}
+      ${generateProductUrls()}
+    </urlset>
+  `
 }
 
 function SiteMap() {
@@ -61,6 +69,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
   // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(blogs, products)
+  console.log('sitemap', sitemap)
 
   res.setHeader('Content-Type', 'text/xml')
   // we send the XML to the browser
